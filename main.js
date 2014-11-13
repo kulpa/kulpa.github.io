87fcfb6251a4b098e917318ab1d3dcad08e1cb85
伊藤航大0815 <itokoudai@itouwatarudai-no-MacBook-Air.local>
2014-11-13 20:31:46 +0900

    comment deleted

--- main.js
+++ main.js
@@ -8,12 +8,8 @@ var number_of_server = 3;           //サーバー数
 var arriving_rate = 36;              //到着率
 var management = 16;                 //処理能力
 var field = 0;                      //その時々のいる人数の状態を格納
-//var new_coming = -(1/arriving_rate)*Math.log(Math.random());
 
 window.onload = function() {
-    //var field = new Array(SIDE_CELLS*SIDE_CELLS); // フィールド情報
-    //var tempField = new Array(SIDE_CELLS*SIDE_CELLS); // フィールド情報の一時記憶用
-    //for (var i=0; i<field.length; i++) field[i] = Math.floor(Math.random()*2); // ランダムに「生」「死」を格納
     canvas = document.getElementById('world'); // canvas要素を取得
     canvas.width = canvas.height = SCREEN_SIZE; // キャンバスのサイズを設定
     var scaleRate = Math.min(window.innerHeight/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE); // Canvas引き伸ばし率の取得
@@ -33,42 +29,6 @@ function update(field){
     draw(field,number_of_server);
     setTimeout(update, 1000/FPS, field);
 }
-
-/**function update() {
-    var n = 0;                    // 自身のまわりにある「生」の数
-    tempField = field.slice(); // 複製
-    for (var i=0; i<tempField.length; i++) {
-        n = 0;
-        for (var s=-1; s<2; s++) {
-            for (var t=-1; t<2; t++) {
-                if (s==0 && t==0) continue; // 自身はカウントしない
-                var c = i+s*SIDE_CELLS+t;   // チェックするセル
-                if (c>=0 && c<tempField.length) { // 配列からはみ出していないか(上下の壁判定も兼ねて)
-                    if (i<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) { // 左右の壁判定
-                        if (tempField[c]) n ++; // 「生」だったらカウント
-                    }
-                }
-            }
-        }
-        if (tempField[i] && (n==2||n==3)) { // 自身が「生」でカウントが2か3
-            field[i] = 1;    // 「生」
-        } else if (!tempField[i] && n==3) { // 自身が「死」でカウントが3
-            field[i] = 1;    // 「生」
-        } else field[i] = 0; // 「死」
-    }
-    draw(field);                                    // canvasを更新
-    setTimeout(update, 1000/FPS, field, tempField); // 再帰
-}**/
-
-/**
-function draw(field) {
-    context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE); // 画面をクリア
-    for (var i=0; i<field.length; i++) {
-        var x = (i%SIDE_CELLS)*CELL_SIZE;             // x座標
-        var y = Math.floor((i/SIDE_CELLS))*CELL_SIZE; // y座標
-        if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE); // 「生」を描画
-    }
-}**/
 
 function draw(field,number_of_server){
     context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE); // 画面をクリア
